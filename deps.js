9f4dcde1f4b005aa51bce5297fcd5aa62968e0a7
aricallen <aric.allen2@gmail.com>
2021-01-30 10:53:51 -0800

    update jest config

--- deps.js
+++ deps.js
@@ -21,3 +21,8 @@ module.exports = {
     'typescript',
   ]
 };
+
+/**
+ * command to install:
+ * yarn add --dev @types/jest @types/node @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-airbnb-base eslint-config-prettier eslint-plugin-import eslint-plugin-node eslint-plugin-prettier husky jest prettier pretty-quick ts-jest ts-node tslib typescript
+ */
